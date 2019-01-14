5fdb7f53abaf6f921bb475df3df75cdd383c6394
Yingqi Chen <chanwk18@hotmail.com>
2019-01-14 20:22:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  var array2=array.slice(1);
-  return array;
+var array2=array.slice(1);
+return array2;
 }
